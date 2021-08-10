d8ea3a9f241845da46da2e7dec39d0ce36ad7a3d
arbeers1 <arbeers@wisc.edu>
2021-08-10 12:52:09 -0500

    Youtube now opens via phone command

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+import sys
+sys.path.append(sys.path[0] + '/src/')
+from src.remote import Remote
 
+remote = Remote()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/youtube')
+def youtube():
+    remote.youtube()
+    return jsonify(result='success')
