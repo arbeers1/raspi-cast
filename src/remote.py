a7598b90a530a4417b20c3f358e7ff1ad2cd7f75
arbeers1 <arbeers@wisc.edu>
2021-08-09 19:18:33 -0500

    Added profile data for auto link on youtube start

--- src/remote.py
+++ src/remote.py
@@ -8,13 +8,20 @@ from selenium.webdriver.chrome.options import Options
 class Remote:
 
     def __init__(self):
+        """
+        Creates a new web driver which can be called upon.
+        """
         options = Options()
         options.add_argument('user-agent=Mozilla/5.0 (Linux; Tizen 2.3 ) AppleWebKit/538.1 (KHTML, like Gecko) Version/2.3 TV Safari/538.1')
         options.add_argument('--start-maximized')
+        options.add_argument('user-data-dir=/home/pi/.config/chromium')
         options.add_experimental_option('excludeSwitches', ['enable-automation'])
         self.driver = webdriver.Chrome(executable_path='/lib/chromium-browser/chromedriver', options=options)
     
     def youtube(self):
+        """
+        Opens youtube tv app in the current window.
+        """
         self.driver.get('https://www.youtube.com/tv')
 
     def netflix(self):
